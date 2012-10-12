5251d15d4011530ae46e49b7c9a1743204a0a982
Syohei YOSHIDA <syohex@gmail.com>
2012-10-12 22:49:32 +0900

    init repos

---
+++ lib/Math/Normalize/Range.pm
@@ -0,0 +1,42 @@
+package Math::Normalize::Range;
+use strict;
+use warnings;
+use 5.008_001;
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Math::Normalize::Range -
+
+=head1 SYNOPSIS
+
+  use Math::Normalize::Range;
+
+=head1 DESCRIPTION
+
+Math::Normalize::Range is
+
+=head1 AUTHOR
+
+Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2012- Syohei YOSHIDA
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
